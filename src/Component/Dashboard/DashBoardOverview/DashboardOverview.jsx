d3a8cad6348467da87c54b10bb2b0bcb5d1ce3e8
sudhanshu-singh5 <sudhanshussingh58@gmail.com>
2022-04-27 14:46:18 +0530

    added device component

--- src/Component/Dashboard/DashBoardOverview/DashboardOverview.jsx
+++ src/Component/Dashboard/DashBoardOverview/DashboardOverview.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DevicesOverview from "../../Devices/DevicesOverview";
 import Navbar from "../DashboardNav/Navbar";
 
 export default function DashBoardOverview() {
@@ -9,18 +10,27 @@ export default function DashBoardOverview() {
           height: "100%",
           backgroundColor: "#ffffff",
           boxShadow: "20px 65px 60px rgba(0, 0, 0, 0.2)",
-          padding: '20px 30px',
-          display: 'flex'
+          padding: "20px 30px",
+          display: "flex",
         }}
       >
-          <div style={{width: '10%', fontWeight: '600'}}>
-              <Navbar />
-
+        <div style={{ width: "180px" }}>
+          <Navbar />
+        </div>
+        <div style={{ color: "black", width: "85%", fontWeight: "500" }}>
+          <div style={{ height: '180px'}}>
+            <p>title</p>
           </div>
-          <div>
-
+          <div style={{margin: '15px'}}>
+            <h1>Devices</h1>
+            <DevicesOverview
+              devices={[
+                { id: 1, name: "d1" },
+                { id: 2, name: "d2" },
+              ]}
+            />
           </div>
-
+        </div>
       </div>
     </>
   );
