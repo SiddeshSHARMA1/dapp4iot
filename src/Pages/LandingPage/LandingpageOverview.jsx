6839ab021ba9b8bb78f5145df569ab9dcec7c1c3
Siddesh-Sharma <siddeshsharma1999@gmail.com>
2022-06-02 12:18:05 +0530

    added authentiction for google

--- src/Pages/LandingPage/LandingpageOverview.jsx
+++ src/Pages/LandingPage/LandingpageOverview.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { signInWithGoogle } from "../../Utilities/firebase";
 
 export default function LandingpageOverview() {
   return (
     <div>
       <p>landing page</p>
+      <button onClick={() => signInWithGoogle()}>SignIn With google</button>
     </div>
   );
 }
--- src/Utilities/firebase.js
+++ src/Utilities/firebase.js
@@ -1,17 +1,68 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAvNTndrygQbuuLUtJwrWuf1K4BRJpj5vw",
-  authDomain: "iotmanager-18e93.firebaseapp.com",
-  projectId: "iotmanager-18e93",
-  storageBucket: "iotmanager-18e93.appspot.com",
-  messagingSenderId: "476706908535",
-  appId: "1:476706908535:web:c8572765d769ee437e5972"
+  apiKey: "AIzaSyCuHCN25WRQ7THUy2paezcYmx0reLd_SbM",
+  authDomain: "dauthclient.firebaseapp.com",
+  projectId: "dauthclient",
+  storageBucket: "dauthclient.appspot.com",
+  messagingSenderId: "588055944940",
+  appId: "1:588055944940:web:3f8f6db83afd881eff0fc9"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+
+export const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    console.log(user);
+    document.cookie = `authToken=${await user.getIdToken()}`;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (error) {
+    console.log("some error occured in auth", error);
+  }
+};
+
+export const logOut = () => {
+  signOut(auth);
+}
+
+
+
