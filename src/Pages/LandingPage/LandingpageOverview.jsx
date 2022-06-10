29823ed0b4ffd444b18416b98cb5849afd34ce36
sudhanshu-singh5 <sudhanshussingh58@gmail.com>
2022-06-10 22:46:14 +0530

    revamped login page

--- src/Pages/LandingPage/LandingpageOverview.jsx
+++ src/Pages/LandingPage/LandingpageOverview.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import { useNavigate } from "react-router";
 import { queryServer, signInWithGoogle } from "../../Utilities/firebase";
@@ -12,10 +13,34 @@ export default function LandingpageOverview({ setLoggedInUser }) {
   }
 
   return (
-    <div>
-      <p>landing page</p>
-      <button onClick={() => handleSignIn()}>SignIn With google</button>
-      <button onClick={() => queryServer()}>qer</button>
+    <div
+      style={{
+        height: "100vh",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <h1 style={{ color: "black", fontFamily: 'arial', fontSize: '48px' }}>DAuth...</h1>
+
+        <Button
+          style={{ border: "1px solid black", borderRadius: "10px" }}
+          onClick={() => handleSignIn()}
+        >
+          <img
+            height={15}
+            width={15}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2048px-Google_%22G%22_Logo.svg.png"
+          />
+          <span
+            style={{ fontFamily: "arial", fontSize: "14px", margin: "4px" }}
+          >
+            Continue with google
+          </span>
+        </Button>
+      </div>
     </div>
   );
 }
